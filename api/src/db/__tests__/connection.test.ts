76f675a5018899e983f723c41899778f3e8070a1
Anshul <anshulkumarg0re@gmail.com>
2025-12-10 16:02:49 +0530

    test(connection.ts): wrote tests for api/src/db/connection.ts

--- api/src/db/__tests__/connection.test.ts
+++ api/src/db/__tests__/connection.test.ts
@@ -7,8 +7,11 @@ beforeEach(() => {
   jest.resetModules();
 });
 
+jest.unstable_mockModule("../../middleware/LoggingRequests.ts", () => ({
+  displayLog: jest.fn()
+}));
 // mocking pg so we dont goof the db 
-jest.mock("pg", () => {
+jest.unstable_mockModule("pg", () => {
 
   const returnclient = {
     connect: jest.fn(),
@@ -18,8 +21,6 @@ jest.mock("pg", () => {
   return { Client: jest.fn(() => returnclient) };
 });
 
-jest.mock("../../middleware/LoggingRequests.ts");
-import { displayLog } from '../../middleware/LoggingRequests.ts';
 
 const dummyDB: DatabaseConfig = {
   PG_URL: "localhost",
@@ -29,9 +30,10 @@ const dummyDB: DatabaseConfig = {
   PG_SECRET: "postgrespassword",
 };
 
-describe("connectDB", () => {
+describe("connectDB tests", () => {
 
   test("returns client on success", async () => {
+    const { displayLog } = await import('../../middleware/LoggingRequests.ts');
     const { connectDB } = await import('../connection.ts');
 
     const db_client = await connectDB(dummyDB);
@@ -44,6 +46,7 @@ describe("connectDB", () => {
   });
 
   test("returns null on client.connect() fails with Aggregate Error", async () => {
+    const { displayLog } = await import('../../middleware/LoggingRequests.ts');
     const { Client } = await import("pg");
     const { connectDB } = await import("../connection.ts");
 
@@ -58,10 +61,11 @@ describe("connectDB", () => {
 
     expect(result).toBeFalsy();
     expect(result).toBeNull();
-    expect(displayLog).toHaveBeenCalled();
+    expect(displayLog as ReturnType<typeof jest.spyOn>).toHaveBeenCalled();
   });
 
   test("returns null on client.connect() fails with Error", async () => {
+    const { displayLog } = await import('../../middleware/LoggingRequests.ts');
     const { Client } = await import("pg");
     const { connectDB } = await import("../connection.ts");
 
@@ -78,5 +82,52 @@ describe("connectDB", () => {
     expect(displayLog).toHaveBeenCalled();
   });
 
+
+});
+
+const error: Error = new Error();
+error.name = "TESTERROR1"
+error.message = "TESTERROR1"
+
+describe("handleDatabaseError tests", () => {
+
+  test("testing error handling", async () => {
+    const { displayLog } = await import("../../middleware/LoggingRequests.ts");
+    const { handleDatabaseError } = await import("../connection.ts");
+
+    handleDatabaseError(error);
+    expect(displayLog).toHaveBeenCalled();
+    expect(displayLog).toHaveBeenCalledTimes(1);
+    expect(displayLog).toHaveBeenCalledWith(error.name + " : " + error.message, 'ERR');
+  });
+
+});
+
+describe("getDBClient tests", () => {
+
+  test("client does not exist", async () => {
+    const { getDBClient } = await import("../connection.ts");
+    const { Client } = await import("pg");
+
+    const result = getDBClient();
+
+    expect(result).toBeDefined();
+    expect(result).not.toBeInstanceOf(Client);
+    expect(result).toHaveProperty(["name"]);
+    expect(result).toHaveProperty(["message"]);
+    expect((result as Error).name).toMatch("Error");
+    expect((result as Error).message).toMatch("Client does not exist yet. DB Connection not made.")
+  });
+
+  test("client exists", async () => {
+    const { connectDB } = await import("../connection.ts");
+    const { getDBClient } = await import("../connection.ts");
+
+    const db_client = await connectDB(dummyDB);
+    const client = getDBClient();
+
+    expect(client).toBeDefined();
+    expect(client).toStrictEqual(db_client);
+  });
 
 });
